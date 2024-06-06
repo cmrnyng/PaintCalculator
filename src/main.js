ecc597137734b44d06467edad40928cff909aca5
Cameron Young <cameron.kamil.young@gmail.com>
2024-06-06 18:10:28 +0100

    project setup w inquirer.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,53 @@
-"use strict";
-const test = (x, y) => {
-    return x + y;
-};
-console.log(test(5, 2));
+import inquirer from "inquirer";
+class Wall {
+    len;
+    height;
+    colour;
+    excludedLen;
+    excludedHeight;
+    constructor(len, height, colour, excludedLen = 0, excludedHeight = 0) {
+        this.len = len;
+        this.height = height;
+        this.colour = colour;
+        this.excludedLen = excludedLen;
+        this.excludedHeight = excludedHeight;
+    }
+    calculateArea() {
+        return this.len * this.height - this.excludedLen * this.excludedHeight;
+    }
+}
+const walls = [];
+console.log("\nHey there! Welcome to Paint It All.\n");
+const initialQuestions = await inquirer.prompt([
+    {
+        // First question
+        type: "input",
+        name: "userName",
+        message: "What is your name?",
+    },
+    {
+        // Second question
+        type: "number",
+        name: "walls",
+        // Function to get name of user in previous question
+        message: answers => `Hey ${answers.userName}, enter the total number of walls you would like to paint:`,
+        validate: (input) => {
+            // Validate the input as a number
+            const num = parseInt(input, 10); // Convert input to a base 10 integer, num
+            if (isNaN(num) || num <= 0) {
+                // If num is not a number or num is negative
+                return "Please enter a valid number greater than 0."; // Return with this message
+            }
+            return true; // Otherwise return true, and move onto the next question
+        },
+        filter: (input) => {
+            // Remove any non numeric values
+            const num = parseInt(input, 10); // Convert input to a base 10 integer, num
+            return isNaN(num) || num <= 0 ? "" : input; // If num is not a number of is negative, return an empty string. Else, return input.
+        },
+    },
+]);
+const numOfWalls = initialQuestions.walls;
+const wallQuestions = [];
+for (let i = 0; i < numOfWalls; i++) { }
+//# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,67 @@
-const test = (x: number, y: number): number => {
-  return x + y;
-};
+import inquirer from "inquirer";
 
-console.log(test(5, 2));
+class Wall {
+  len: number;
+  height: number;
+  colour: string;
+  excludedLen: number;
+  excludedHeight: number;
+
+  constructor(
+    len: number,
+    height: number,
+    colour: string,
+    excludedLen: number = 0,
+    excludedHeight: number = 0
+  ) {
+    this.len = len;
+    this.height = height;
+    this.colour = colour;
+    this.excludedLen = excludedLen;
+    this.excludedHeight = excludedHeight;
+  }
+
+  calculateArea() {
+    return this.len * this.height - this.excludedLen * this.excludedHeight;
+  }
+}
+
+const walls: Wall[] = [];
+
+console.log("\nHey there! Welcome to Paint It All.\n");
+
+const initialQuestions = await inquirer.prompt([
+  {
+    // First question
+    type: "input",
+    name: "userName",
+    message: "What is your name?",
+  },
+  {
+    // Second question
+    type: "number",
+    name: "walls",
+    // Function to get name of user in previous question
+    message: answers =>
+      `Hey ${answers.userName}, enter the total number of walls you would like to paint:`,
+    validate: (input: string) => {
+      // Validate the input as a number
+      const num = parseInt(input, 10); // Convert input to a base 10 integer, num
+      if (isNaN(num) || num <= 0) {
+        // If num is not a number or num is negative
+        return "Please enter a valid number greater than 0."; // Return with this message
+      }
+      return true; // Otherwise return true, and move onto the next question
+    },
+    filter: (input: string) => {
+      // Remove any non numeric values
+      const num = parseInt(input, 10); // Convert input to a base 10 integer, num
+      return isNaN(num) || num <= 0 ? "" : input; // If num is not a number of is negative, return an empty string. Else, return input.
+    },
+  },
+]);
+
+const numOfWalls = initialQuestions.walls;
+
+const wallQuestions: object[] = [];
+for (let i = 0; i < numOfWalls; i++) {}
